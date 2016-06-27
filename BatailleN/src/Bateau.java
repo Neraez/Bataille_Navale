2754babf5a7db6457fafa04ff779cef3ecd4e36a
Quentin <Quentin@pcquentin>
2016-06-27 14:33:16 +0200

    Màj

--- BatailleN/src/Bateau.java
+++ BatailleN/src/Bateau.java
@@ -9,36 +9,7 @@ public class Bateau {
                 element[i]=new Element(x+i,y);
             }
         }else{
-            for(int i=0;i<taille;i++){
-                element[i]=new Element (x,y+i);
-            }
-        }
-    }
-    public void avancer(int x,int y){
-        for(int i=0;i<element.length;i++){
-            element[i].avancer(x,y);
-        }
-    }
-    public void touche(int x, int y){
-        for(int i=0;i<element.length;i++){
-            element[i].touche(x,y);
-        }
-    }
-    public boolean estdetruit(){
-        boolean res=true;
-        int i=0;
-        while((i<element.length)&&res){
-            res=(res)&&(element[i].getEtat()=="detruit");
-            i++;
-        }
-        return res;
-    }
-    public String toString(){
-        String s="("+element[0].getabcisse()+","+element[0].getordonnee()+")";
-        for(int i=0;i<element.length;i++)
-            s+=element[i].toString();
-        return s;
-    }
+            
     }
     
 
