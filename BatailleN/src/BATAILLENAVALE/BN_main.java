e67500c4ffa99addd96cdfbf36b9ae2d4030de63
Erwan <Erwan@192.168.88.138>
2016-06-27 14:17:42 +0200

    Ajout de l'escorteur

---
+++ BatailleN/src/BATAILLENAVALE/BN_main.java
@@ -0,0 +1,18 @@
+package BATAILLENAVALE;
+
+public class BN_main {
+
+	public static void main(String[] args) {
+		
+		/*
+		Je suis un code.
+		Je vais devoir me modifier.
+		Parce que je suis censé être un jeu.
+		Un jeu de bataille navale.
+		Mais pour l'instant il n'y a rien.
+		Donc pour l'instant je ne fonctionne pas.
+		Mais cela ne devrait pas tarder ....
+		*/
+	}
+
+}
